92c3b1fc1d508898a9670fda836b03f502a016f8
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2016-02-26 12:15:15 +0100

    Allow creation via PUT as well and implement GET

--- alexandria.py
+++ alexandria.py
@@ -11,15 +11,29 @@ class Alexandria:
         r = requests.get(self.server + "/about", timeout=5.0)
         return r
 
-    def register_resource(self, uri):
-        ref = {'ref': uri}
-        payload = {'resource': ref}
-        r = self.__post("/resources", payload)
-        if r.status_code == 201:
-            location = r.headers['location']
-            return location.split('/')[-1]
+    def get_resource(self, uuid):
+        return self.__get("/resources/" + uuid).json()
+
+    def register_resource(self, uri, uuid=None):
+        payload = {'resource': {'ref': uri}}
+
+        if uuid is None:
+            r = self.__post("/resources", payload)
+            if r.status_code == 201:  # Created
+                location = r.headers['location']
+                return location.split('/')[-1]
         else:
-            raise Exception("Failed to register resource", r.status_code, r.json()['error']['message'])
+            r = self.__put("/resources/" + uuid, payload)
+            if r.status_code == 204:  # No Content
+                return uuid
+
+        raise Exception("Failed to register resource", r.status_code, r.json()['error']['message'])
+
+    def __get(self, endpoint):
+        return self.__request(method='get', endpoint=endpoint)
+
+    def __put(self, endpoint, payload):
+        return self.__request(method='put', endpoint=endpoint, payload=payload)
 
     def __post(self, endpoint, payload):
         return self.__request(method='post', endpoint=endpoint, payload=payload)
@@ -27,7 +41,7 @@ class Alexandria:
     def __delete(self, endpoint):
         return self.__request(method='delete', endpoint=endpoint)
 
-    def __request(self, method, endpoint, payload):
+    def __request(self, method, endpoint, payload=None):
         url = self.server + endpoint
         headers = {'x-ssl-client-s-dn-cn': self.auth}
         return requests.request(method=method, url=url, headers=headers, json=payload)
--- main.py
+++ main.py
@@ -18,7 +18,11 @@ def main(argv):
     result = r.json()
     print(json.dumps(result))
 
-    a8a.register_resource("http://www.example.com/some/resource")
+    # a8a.register_resource("http://www.example.com/some/resource")
+    uuid = a8a.register_resource("http://www.example.com/bogus/resource", "d224ff81-b26f-4fce-a54f-56c74f6819d6")
+
+    res = a8a.get_resource(uuid)
+    print(res)
 
 
 if __name__ == "__main__":
