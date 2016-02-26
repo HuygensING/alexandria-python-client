a2ce5155ffc281d8cbabdb519a8f91ecf747db9a
Hayco de Jong <hayco.de.jong@huygens.knaw.nl>
2016-02-26 11:49:53 +0100

    [WIP] Add Alexandria class to wrap calls to alexandria server
    Also add some command-line parsing.

--- alexandria.py
+++ alexandria.py
@@ -1,8 +1,33 @@
+import json
 import requests
 
-_server = 'http://tc23.huygens.knaw.nl/test-alexandria'
 
+class Alexandria:
+    def __init__(self, server, auth):
+        self.server = server
+        self.auth = auth
 
-def about(server=_server):
-    r = requests.get(server + "/about", timeout=5.0)
-    return r
+    def about(self):
+        r = requests.get(self.server + "/about", timeout=5.0)
+        return r
+
+    def register_resource(self, uri):
+        ref = {'ref': uri}
+        payload = {'resource': ref}
+        r = self.__post("/resources", payload)
+        if r.status_code == 201:
+            location = r.headers['location']
+            return location.split('/')[-1]
+        else:
+            raise Exception("Failed to register resource", r.status_code, r.json()['error']['message'])
+
+    def __post(self, endpoint, payload):
+        return self.__request(method='post', endpoint=endpoint, payload=payload)
+
+    def __delete(self, endpoint):
+        return self.__request(method='delete', endpoint=endpoint)
+
+    def __request(self, method, endpoint, payload):
+        url = self.server + endpoint
+        headers = {'x-ssl-client-s-dn-cn': self.auth}
+        return requests.request(method=method, url=url, headers=headers, json=payload)
--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from alexandria import *
+import alexandria
+import argparse
+import json
+import sys
 
-r = about()
-print(r.url)
-print(r.status_code)
-print(r.headers['content-type'])
-print(r.json())
+
+def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--auth", dest="auth", help="Local authentication token")
+    parser.add_argument("-k", "--key", dest="key_file", help='Client Certificate Private Key file')
+    parser.add_argument("-c", "--cert", dest="cert_file", help='Client Certificate file')
+    parser.add_argument("-s", "--server", dest="server", help='Base URI of Alexandria server')
+    args = parser.parse_args(argv)
+    print(args)
+
+    a8a = alexandria.Alexandria(args.server, args.auth)
+    r = a8a.about()
+    result = r.json()
+    print(json.dumps(result))
+
+    a8a.register_resource("http://www.example.com/some/resource")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
