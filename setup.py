5a21d1aee6dd2aacb229f82e165facf0c6ccbf5a
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2016-09-08 17:03:36 +0200

    Fixes setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 
 from distutils.core import setup
 
-setup(name='Alexandria',
+setup(name='AlexandriaClient',
       version='1.0',
-      description='Alexandria tools',
+      description='Client to interact with alexandria server',
       author='HuygensING',
       author_email='alexandria@huygens.knaw.nl',
-      packages=['alexandria']
+      packages=['alexandria.client']
       )
